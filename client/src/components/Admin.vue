e4e567b5f1b379ad7409ae3c0729b70224e7c57a
Joshua Teague Van Pelt <joshuavanp@gmail.com>
2019-06-17 12:19:12 -0600

    Renamed "Display/Addon View" to "Gui Transaction"

--- client/src/components/Admin.vue
+++ client/src/components/Admin.vue
@@ -6,7 +6,7 @@
   <router-link v-bind:to="{ name: 'Manage', params: { view: true } }"><button class="admin-button">Manage Data</button></router-link>
   <router-link to="/display"><button class="admin-button">Current/Previous Sale Display</button></router-link>
   <router-link to="/exhibitor/history"><button class="admin-button">Exhibitor History Display</button></router-link>
-  <router-link to="/buyer"><button class="admin-button">Buyer/Addons Display</button></router-link>
+  <router-link to="/transaction/gui"><button class="admin-button">GUI Transaction</button></router-link>
   <router-link to="/transaction/buyer"><button class="admin-button">Buyer Transaction Table</button></router-link>
   <router-link to="/transaction/addon"><button class="admin-button">Addon Transaction Table</button></router-link>
   <router-link to="/admin" class-active="active" v-on:click.native="logout()" replace><button class="admin-button-logout">Logout</button></router-link>
