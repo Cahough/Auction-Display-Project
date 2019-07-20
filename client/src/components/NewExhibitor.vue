691232c8c824b78fbebfd8fbff59e0f3914d22aa
Joshua Teague Van Pelt <joshuavanp@gmail.com>
2019-07-20 11:14:37 -0600

    Edited validation

--- client/src/components/NewExhibitor.vue
+++ client/src/components/NewExhibitor.vue
@@ -4,25 +4,25 @@
     <div v-if="this.isDataReady">
       <label v-if="duplicateSaleNumber" class="errorLabel" for="saleNumber">Error: Duplicate Sale Number. Sale Number must be unique.</label>
       <label v-else class="errorLabel" for="saleNumber" >{{ errors.first('saleNumber') }}</label>
-      <input v-validate="'required|numeric'" type="text" name="saleNumber" :placeholder="'Sale Number (next available: ' + nextAvailableSaleNumber + ')'" v-model="saleNumber">
+      <input v-validate="'required|numeric'" type="text" name="saleNumber" :placeholder="'Sale Number (next available: ' + nextAvailableSaleNumber + ')'" v-model="saleNumber" autocomplete="off">
       <label class="errorLabel" for="fullName" >{{ errors.first('fullName') }}</label>
-      <input v-validate="'required|alpha_spaces'" type="text" name="fullName" placeholder="Full Name" v-model=fullName>
-      <label class="errorLabel" for="tag" >{{ errors.first('tag') }}</label>
-      <input v-validate="'required'" type="text" name="tag" placeholder="Tag" v-model="tag">
+      <input v-validate="'required|alpha_spaces'" type="text" name="fullName" placeholder="Full Name" v-model=fullName autocomplete="off">
       <label class="errorLabel" for="species" >{{ errors.first('species') }}</label>
-      <input v-validate="'required|alpha'" type="text" name="species" placeholder="Species" v-model="species">
+      <input v-validate="'required|alpha_spaces'" type="text" name="species" placeholder="Species" v-model="species" autocomplete="off">
       <label class="errorLabel" for="checkInWeight" >{{ errors.first('checkInWeight') }}</label>
-      <input v-validate="'required|decimal'" type="text" name="checkInWeight" placeholder="Check-In Weight" v-model=checkInWeight>
+      <input v-validate="'required|decimal'" type="text" name="checkInWeight" placeholder="Check-In Weight" v-model=checkInWeight autocomplete="off">
+      <label class="errorLabel" for="tag" >{{ errors.first('tag') }}</label>
+      <input v-validate="''" type="text" name="tag" placeholder="Tag" v-model="tag" autocomplete="off">
       <label class="errorLabel" for="animalDescription" >{{ errors.first('animalDescription') }}</label>
-      <input v-validate="''" type="text" name="animalDescription" placeholder="Animal Description" v-model="animalDescription">
+      <input v-validate="''" type="text" name="animalDescription" placeholder="Animal Description" v-model="animalDescription" autocomplete="off">
       <label class="errorLabel" for="clubName" >{{ errors.first('clubName') }}</label>
-      <input v-validate="''" type="text" name="clubName" placeholder="Club Name" v-model=clubName>
+      <input v-validate="''" type="text" name="clubName" placeholder="Club Name" v-model=clubName autocomplete="off">
       <label class="errorLabel" for="showClassName" >{{ errors.first('showClassName') }}</label>
-      <input v-validate="'alpha_spaces'" type="text" name="showClassName" placeholder="Show Class Name" v-model="showClassName">
+      <input v-validate="'alpha_spaces'" type="text" name="showClassName" placeholder="Show Class Name" v-model="showClassName" autocomplete="off">
       <label class="errorLabel" for="placing" >{{ errors.first('placing') }}</label>
-      <input v-validate="''" type="text" name="placing" placeholder="Placing" v-model=placing>
+      <input v-validate="''" type="text" name="placing" placeholder="Placing" v-model=placing autocomplete="off">
       <label class="errorLabel" for="buyback" >{{ errors.first('buyback') }}</label>
-      <input v-validate="'numeric'" type="text" name="buyback" placeholder="Buyback" v-model=buyback>
+      <input v-validate="'numeric'" type="text" name="buyback" placeholder="Buyback" v-model=buyback autocomplete="off">
       <div class="confirmLabelContainer"><label id="confirmLabel"></label></div>
       <button class="manage__button" @click=validate>Add</button>
       <router-link v-bind:to="{ name: 'Manage', params: {view: true } }">
@@ -86,12 +86,27 @@ export default {
         this.exhibitors = response.data
         })
     },
-    validate () {
+    async validate () {
       this.$validator.validateAll()
       if (!this.errors.any() && !this.duplicateSaleNumber) {
         this.confirmAdd()
-        this.addExhibitor()
-        this.resetExhibitor()
+        await this.addExhibitor()
+          .then(() => { this.resetExhibitor() })
+          .catch(err => {
+            let newExhibitor = {
+              saleNumber: this.saleNumber,
+              fullName: this.fullName,
+              tag: this.tag,
+              species: this.species,
+              animalDescription: this.animalDescription,
+              checkInWeight: this.checkInWeight,
+              clubName: this.clubName,
+              showClassName: this.showClassName,
+              placing: this.placing,
+              buyback: this.buyback
+            }
+            window.alert(err + "\n" + newExhibitor)
+          })
       }
     },
     async addExhibitor () {
